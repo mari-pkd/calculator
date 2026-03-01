737862248f43107951f4b1c3e62cca9abfe210e1
mariP <200~259964306+mari-pkd@users.noreply.github.com>
2026-03-01 19:44:40 +0300

    Add display functionality

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const validNumbers = "1234567890.";
-const validOperators = "-+*/^";
+const validOperators = ["+", "-", "*", "/", "^"];
 
-const numFirst = "";
-const operator = "";
-const numSecond = "";
+const buttons = document.querySelector(".buttons");
+const display = document.querySelector(".display");
+
+let nums = [];
+let numFirst = "";
+let operator = "";
+let numSecond = "";
 
 
 function add(a, b) {
@@ -25,3 +29,47 @@ function divide(a, b) {
 function toPower(a, b) {
     return numFirst = a ** b;
 }
+
+function operate(a, operator, b) {
+    switch (operator) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break
+        case "^":
+            toPower(a, b);
+    }
+}
+
+function splitNums(nums) {
+    let operatorIndex = nums.findLastIndex((operator) => {
+        return validOperators.includes(operator);
+    })
+    numFirst += nums.slice(0, operatorIndex).join("");
+    numSecond += nums.slice(++operatorIndex).join("");
+    operator = nums[--operatorIndex];
+    operate(+numFirst, operator, +numSecond);
+    display.value = numFirst;
+    console.log(numFirst);
+}
+
+buttons.addEventListener("click", (event) => {
+    let target = event.target;
+    if (target.id == "equal") {
+        splitNums(nums);
+    } else
+        {nums.push(target.textContent);
+        display.value = nums.join("");
+        console.log(nums);}
+
+
+
+})
