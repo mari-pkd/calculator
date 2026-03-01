b9cca71cfb5f7ef75612ad7b7f7d712065727131
mariP <200~259964306+mari-pkd@users.noreply.github.com>
2026-03-01 17:59:22 +0300

    Set up project

---
+++ script.js
@@ -0,0 +1,27 @@
+const validNumbers = "1234567890.";
+const validOperators = "-+*/^";
+
+const numFirst = "";
+const operator = "";
+const numSecond = "";
+
+
+function add(a, b) {
+    return numFirst = a + b;
+}
+
+function subtract(a, b) {
+    return numFirst = a - b;
+}
+
+function multiply(a,b) {
+    return numFirst = a * b;
+}
+
+function divide(a, b) {
+    return numFirst = a / b;
+}
+
+function toPower(a, b) {
+    return numFirst = a ** b;
+}
